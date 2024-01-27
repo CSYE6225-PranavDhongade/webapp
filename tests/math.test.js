8825fa18736dabb29d7f17357237585a18a3b617
pranavmakarand <pranavmakarand94@gmail.com>
2024-01-26 20:03:10 -0500

    added unit test

--- tests/math.test.js
+++ tests/math.test.js
@@ -4,8 +4,3 @@ const { add } = require('../math');
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
-
-// Test case 2
-test('adds -1 + 1 to equal 0', () => {
-  expect(add(-1, 1)).toBe(0);
-});
