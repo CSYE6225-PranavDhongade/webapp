2e165185585b9bce12861fd3d7810dcc9bd3c8f8
pranavmakarand <pranavmakarand94@gmail.com>
2024-04-20 20:44:38 -0400

    pushing env

--- config/sequelizeconfig.js
+++ config/sequelizeconfig.js
@@ -1,12 +1,19 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+const dbPort = process.env.PORT;
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'cloudassignmentdatabase',
+  host: dbHost,
+  port: dbPort,
+  username: dbUser,
+  password: dbPass,
+  database: dbName,
   define: {
     timestamps: false,
   },
